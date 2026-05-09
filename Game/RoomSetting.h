e778060c87f116f6f3e1a1cbbe37030ab7f4e525
PPAK_JU <153336193+taku7664@users.noreply.github.com>
2026-05-09 16:30:47 +0900

    fix: 방 설정 비교 누락 수정

--- Game/RoomSetting.h
+++ Game/RoomSetting.h
@@ -7,15 +7,16 @@ struct RoomSetting
 	int		Col = 8;
 	int		Timer = 30;
 
-	bool operator == ( const RoomSetting& other )
+	bool operator == ( const RoomSetting& other ) const
 	{
 		return MaxPlayerCount == other.MaxPlayerCount &&
 			MaxCycle == other.MaxCycle &&
 			Row == other.Row && 
-			Col == other.Col;
+			Col == other.Col &&
+			Timer == other.Timer;
 	}
 
-	bool operator != ( const RoomSetting& other )
+	bool operator != ( const RoomSetting& other ) const
 	{
 		return !operator==( other );
 	}
